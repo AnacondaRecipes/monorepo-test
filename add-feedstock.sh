b98c2a904b117c09c47b178be8fa0d0259762e15
Daniel Komisar <dkomisar@anaconda.com>
2022-10-03 14:39:57 -0400

    exit early

--- add-feedstock.sh
+++ add-feedstock.sh
@@ -2,6 +2,6 @@
 
 feedstock=$1
 
-git remote add $feedstock git@github.com:AnacondaRecipes/$feedstock
+git remote add $feedstock git@github.com:AnacondaRecipes/$feedstock || exit 1
 git config --local remote.$feedstock.fetch "+refs/heads/*:refs/feedstocks/$feedstock/*"
 git remote update $feedstock
